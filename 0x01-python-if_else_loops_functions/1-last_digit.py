dbee7109565dcb6781d630f62838e1967fc2a3e5
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-11-30 00:35:54 +0000

    Get last digit of a number and compare with certain conditions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 
 if number < 0:
-    last_digit = (-1 * number) % 10
+    last_digit = ((-1 * number) % 10) * -1
 else:
     last_digit = number % 10
 last_digit_string = f"Last digit of {number} is {last_digit}"

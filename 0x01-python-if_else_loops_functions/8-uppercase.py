c36ed6116546a691f69bf073b2f35e0e3aeefd6d
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-11-30 02:56:41 +0000

    Print a string in uppercase followed by a new line

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,5 @@ def uppercase(str):
         ascii_int = ord(s)
         if ascii_int in range(97, 123):
             ascii_int = ascii_int - 32
-            print('{:c}'.format(ascii_int), end="")
-        else:
-            print('{:c}'.format(ascii_int), end="")
+        print('{:c}'.format(ascii_int), end="")
     print('{:c}'.format(10), end="")

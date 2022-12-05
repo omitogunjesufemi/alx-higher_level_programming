719e48b29d7fc4b4b57964d5a6f70b83ddb8846d
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-12-05 08:56:20 +0000

    Print a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    matrix_len = len(matrix)
+
+    if matrix_len > 1:
+        for row in matrix:
+            row_len = len(row)
+            i = 0
+            for item in row:
+                if i < row_len - 1:
+                    print('{:d}'.format(item), end=" ")
+                else:
+                    print('{:d}'.format(item), end="\n")
+                i += 1
+
+    else:
+        print()

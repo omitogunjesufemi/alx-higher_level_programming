e92ef490211bbea5f41bc7f2de492765e00e5b7f
omitogunjesufemi <omitogunjesufemi@gmail.com>
2022-12-06 00:18:33 +0000

    Add two tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,11 +9,16 @@ def add_tuple(tuple_a=(), tuple_b=()):
     result_tuple = ()
 
     if len_a < 2:
-        for i in range(len_a, 2):
-            list_b.insert(i, 0)
+        if len_a == 0:
+            list_a[0:2] = [0, 0]
+        elif len_a == 1:
+            list_a[1] = 0
     if len_b < 2:
-        for i in range(len_b, 2):
-            list_b.insert(i, 0)
+        if len_b == 0:
+            list_b[0:2] = [0, 0]
+        elif len_b == 1:
+            list_b[1:2] = [0]
+
     result = [list_a[x]+list_b[y]
               for x in range(0, 2)
               for y in range(0, 2)

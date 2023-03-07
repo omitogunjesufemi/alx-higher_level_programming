1d8cd59eea565d03763ec40db1f3274a17672bf5
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-03-07 14:35:29 +0000

    Print string in uppercase followed by a new line

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for s in str:
         if ord(s) in range(97, 123):
             s = chr(ord(s) - 32)
-        print(s, end="")
+        print("{}".format(s), end="")
     print(end="\n")

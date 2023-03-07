db16b7e4992dad8e5d80dfbdd26f17d0bbe01d18
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-03-07 13:09:36 +0000

    Prints the ASCII alphabet in lowercase without e and q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for number in range(97, 123):
-    if chr(number) != "e" or chr(number) != "q":
-        print("{}".format(chr(number)), end="")
+    if chr(number) == 'e' or chr(number) == 'q':
+        continue;
+    print("{}".format(chr(number)), end="")

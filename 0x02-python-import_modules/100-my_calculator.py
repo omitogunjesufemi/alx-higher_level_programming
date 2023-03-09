b02ee86f25beb21dcb9d227139407ae0cba9cc0d
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-03-09 14:20:26 +0000

    Python program from bytecode

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -26,7 +26,7 @@ def main(argv):
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             sys.exit(1)
-        print("{:d} {} {:d} = {:d}".format(a, operator, b, result), end="\n")
+        print("{:.0f} {} {:.0f} = {:.0f}".format(a, operator, b, result), end="\n")
 
 
 if __name__ == "__main__":

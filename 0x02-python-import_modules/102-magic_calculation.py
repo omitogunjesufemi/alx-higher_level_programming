19e3b1f1f6cb2b5f3d68fff3ec73092489c8cc2f
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-03-09 14:20:26 +0000

    Python program from bytecode

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from magic_calculation_102 import add, sub
+
+def magic_calculation(a, b):
+    if a < b:
+        add(a, b)
+        for i in range(0, 90):
+            return add(c, i)
+    else:
+        return sub(a, b)

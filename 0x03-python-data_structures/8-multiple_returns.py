6a46bfbeda000e46791a32a5271057bfeac40fa5
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-03-11 11:33:41 +0000

    Renamed folder

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 def multiple_returns(sentence):
-        s_len = len(sentence)
-        if s_len == 0:
-                s_tuple = 0, None
-                return s_tuple
-        s_tuple = s_len, sentence[0]
+    s_len = len(sentence)
+    if s_len == 0:
+        s_tuple = 0, None
         return s_tuple
+    s_tuple = s_len, sentence[0]
+    return s_tuple

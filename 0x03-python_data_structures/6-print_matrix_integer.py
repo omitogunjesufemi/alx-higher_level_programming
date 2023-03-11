20a845933c21b3753afe8311c88872f3e2a16b19
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-03-11 11:22:21 +0000

    Print a matrix

---
+++ 0x03-python_data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j == len(matrix) - 1:
+                print("{}".format(matrix[i][j]), end="")
+            else:
+                print("{}".format(matrix[i][j]), end=" ")
+        print()

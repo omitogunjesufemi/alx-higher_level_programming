8b219d016ea7f8b87b4068c363789e10cd309567
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-03-11 11:22:55 +0000

    Add up elements in a tuple

---
+++ 0x03-python_data_structures/7-add_tuple.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
+
+    if len_a == 1:
+        tuple_a = tuple_a[0], 0
+    elif len_a == 0:
+        tuple_a = 0, 0
+
+    if len_b == 1:
+        tuple_b = tuple_b[0], 0
+    elif len_b == 0:
+        tuple_b = 0, 0
+
+    new_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    return new_tuple

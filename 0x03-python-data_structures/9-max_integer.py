ce79c0d0972f931d87d54518a673fac1d24f56d8
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-03-12 18:25:46 +0000

    Correction on max of integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,10 +2,10 @@
 
 def max_integer(my_list):
     list_len = len(my_list)
-    largest = my_list[0]
 
     if list_len == 0:
         return None
+    largest = my_list[0]
     for i in my_list:
         if i > largest:
             largest = i

bd41262fe73b20b8916caabcfbbf2831cb6fd1c2
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-03-15 17:42:02 +0000

    Adds all unique integers in a list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@
 def search_replace(my_list, search, replace):
     if len(my_list) <= 0:
         return my_list
-    new_list = my_list.copy();
+    new_list = my_list.copy()
     for i in range(len(new_list)):
         if new_list[i] == search:
             new_list[i] = replace

f2b0bf197ec1616c9abe232aa5d27baa17a798ee
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-03-15 20:40:24 +0000

    Converts a Roman numeral to an integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+
 def roman_to_int(roman_string):
+    if roman_string is None or not_a_string(roman_string) == 1:
+        return 0
+
     roman_char = {"I": 1, "V": 5, "X": 10,
                   "L": 50, "C": 100, "D": 500, "M": 1000}
     solve_list = []
@@ -12,3 +16,9 @@ def roman_to_int(roman_string):
                 solve_list[i - 1] = solve_list[i - 1] * (-1)
 
     return (sum(solve_list))
+
+def not_a_string(string):
+    for s in string:
+        if s in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']:
+            return 1
+    return 0

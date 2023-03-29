2dcac72cf3a8925457b8c2a028ef302c29cd67c0
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-03-29 13:45:51 +0000

    Prints an integer and exception when it is not an integer

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except TypeError as te:
+        print("Exception: ",te, file=sys.stderr)
+        return False
+    except ValueError as ve:
+        print("Exception: ", ve, file=sys.stderr)
+        return False

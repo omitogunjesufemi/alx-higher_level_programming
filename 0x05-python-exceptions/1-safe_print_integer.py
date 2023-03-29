a2f2efc5f5e34d7697c106be94c8fbc1ab7473a2
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-03-29 13:44:03 +0000

    Fixed edge case for string value as argument

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,12 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        x = int(value)
-        y = float(value)
-        if x - y == 0:
-            print("{:d}".format(x))
-            return True
-        else:
-            return False
+        print("{:d}".format(value))
+        return True
     except (ValueError, TypeError):
         return False

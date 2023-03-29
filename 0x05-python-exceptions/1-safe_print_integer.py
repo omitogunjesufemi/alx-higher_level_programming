f03381496cd91c6ea4063adb2a07e7490dd539d3
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-03-29 07:36:36 +0000

    Safe printing of an integers list

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        x = int(value)
+        print("{:d}".format(x))
+        return True
+    except ValueError:
+        return False

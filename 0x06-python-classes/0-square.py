47e88df29c9b55e10bf985b5081e2d81d6f63371
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-03-30 00:33:43 +0000

    Added comment through docstring to the start before the class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+This module is to create an empty class that defines a square
+
+Attributes:
+        None
+
+Todo:
+        * Nothing yet
+"""
+
 
 class Square:
     """This is an empty class that defines square

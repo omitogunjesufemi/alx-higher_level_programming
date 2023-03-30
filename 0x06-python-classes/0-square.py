1f51c46d4b2e0157287fae9933e1bed5d926d865
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-03-30 00:25:16 +0000

    Added comment through docstring

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 
 class Square:
+    """This is an empty class that defines square
+
+    This class has no public attributes
+
+    Attributes:
+            None
+    """
     pass

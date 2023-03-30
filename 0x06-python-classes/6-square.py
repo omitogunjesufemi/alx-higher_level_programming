66b60a810431aebe399d035277cdda664509049d
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-03-30 11:08:32 +0000

    Edge cases for negative position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -64,7 +64,8 @@ class Square:
 
         TypeError - position must be a tuple of 2 positive integers
         """
-        if type(value) is tuple and len(value) == 2:
+        if (type(value) is tuple and len(value) == 2
+            and value[0] >= 0 and value[1] >= 0):
             self.__position = value
         else:
             raise TypeError("position must be a tuple of 2 positive integers")

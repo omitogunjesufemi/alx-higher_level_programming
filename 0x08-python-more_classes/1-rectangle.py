751db4bf9e93efc436dfe8017aa223a6ad52a22e
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-04-03 13:29:21 +0000

    Edge case perimeter when width or height is zero

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -40,8 +40,7 @@ class Rectangle:
 
         if type(value) is not int:
             raise TypeError("width must be an integer")
-
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -61,7 +60,6 @@ class Rectangle:
         """
         if type(value) is not int:
             raise TypeError("height must be an integer")
-
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -72,4 +72,6 @@ class Rectangle:
 
     def perimeter(self):
         """This returns the rectangle perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return 2 * (self.__width + self.__height)

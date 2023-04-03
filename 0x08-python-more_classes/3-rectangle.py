d15dd08ccd5951119cf6d6b2bc4e48492a54a643
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-04-03 19:13:27 +0000

    Switched the position of width and height in the initialisation section

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -20,8 +20,8 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-        self.height = height
         self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -40,7 +40,8 @@ class Rectangle:
 
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -60,6 +61,27 @@ class Rectangle:
         """
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    def area(self):
+        """This returns the rectangle area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """This returns the rectangle perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print("#", end="")
+            print("\n")
+        return ""
+
+    def __repr__(self):
+        return type(self)

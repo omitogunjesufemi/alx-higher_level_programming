ee6a12a8e0cb22e8addbca910c8cd52e524a2a4d
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-04-03 12:07:00 +0000

    Create empty class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""This module creates a class for Rectangle class that defines
+a rectangle
+
+Attributes:
+    This is an empty class
+"""
+
+
+class Rectangle:
+    """Rectangle class defines a rectangle
+
+    Attributes:
+        None
+
+    It is an empty class
+    """
+    pass

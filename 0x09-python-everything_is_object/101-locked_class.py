b186df7e0d8cf328921f17c7d466fdcbc0d02474
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-04-05 18:20:00 +0000

    LockedClass

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module is a LockedClass with no class or object attributes
+"""
+
+class LockedClass:
+    """LockedClass with no class or object attribute.
+    """
+    __slots__ = ['first_name']
+    pass

15222191399d5eb7f6b0e4d0eb823d1964cd5ece
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-04-10 21:14:11 +0000

    Bytecode for magic calculation

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module is a replication of the magic calculation from
+a Python Bytecode
+"""
+
+def magic_calculation(a, b):
+    """Magic calculation
+    """
+    result = 0
+
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far");
+            else:
+                result += (a ** b) / i
+        except:
+            result = a + b
+            break
+
+    return result

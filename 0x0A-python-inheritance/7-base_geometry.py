dacf81528724124b218d8d867547c71fd2cfd99c
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-04-11 20:27:26 +0000

    Integer Validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module is creates an empty class:
+   BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry is an empty class
+    """
+    def area(self):
+        """Public instance method that raises an Exception
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Public instance method for validating value
+        """
+        if type(value) is int:
+            pass
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))

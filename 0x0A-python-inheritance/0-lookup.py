c90f2786f7109796cfe2b4950f742359580b4d15
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-04-11 14:53:46 +0000

    Prints a list of available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This modules contains a function that returns the list of
+available attributes and methods of an object
+"""
+
+def lookup(obj):
+    """lookup: A function that returns the list of available
+    attributes and methods of an object
+
+    Return:
+        list object
+    """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,5 @@
+# 0x0A. Python - Inheritance
+
+This project helped understand:
+- superclass, baseclass or parentclass
+- subclass

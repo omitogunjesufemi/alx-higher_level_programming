2cb6b023d142c37e4a6e1000a42877d1752dfe0f
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-04-12 23:02:18 +0000

    Searches and updates a line of text

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -8,8 +8,9 @@ def append_after(filename="", search_string="", new_string=""):
     """A function that inserts a line of text to a file, after each
     line containing a specific string
     """
-    if search_string != "" and new_string != "":
-
+    if search_string == "" or new_string == "":
+        pass
+    else:
         with open(filename, "r+", encoding="utf-8") as f:
             f_content = list(f)
             for i in range(len(f_content)):

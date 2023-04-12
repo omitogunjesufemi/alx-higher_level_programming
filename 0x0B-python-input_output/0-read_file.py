06ee0142cdf85c4a65100f5e9c7478fc5aa0ca37
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-04-12 15:11:50 +0000

    Reads content from a file and prints it to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module contains a function that read a text file with a UTF-8
+encoding and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """A function that read a text file with a UTF-8 encoding algorithm
+    and pirnts it to stdout:
+
+    The with statement is used in this function to open the file
+
+    This function does not manage file permission or other exceptions
+    """
+    with open(filename, "r", encoding="utf-8") as a_file:
+        file_content = a_file.read()
+        print(file_content.strip("\n"))

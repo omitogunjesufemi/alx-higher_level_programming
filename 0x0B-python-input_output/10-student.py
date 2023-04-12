499cb4579a95607ee5817be799d58744f23ce380
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-04-12 20:39:32 +0000

    Modifies the to_json method in Student class

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""This module contains a Student class that defines a student by name and age.
+"""
+
+
+class Student:
+    """Student class the defines a student by:
+
+    Attributes:
+        => first_name
+        => last_name
+        => age
+
+    Method:
+        => to_json
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """This initializes the class with the attributes:
+
+        Attributes:
+            => first_name
+            => last_name
+            => age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """This retrieves a dictionary representation of a Student instance
+
+        attrs: is a list of strings, only attribute names contained
+        in this list must be retrieved
+        """
+
+        my_attrs = list(self.__dict__.keys())
+
+        class_dict = {}
+
+        if attrs is None or my_attrs == attrs:
+            return self.__dict__
+
+        for attr in my_attrs:
+            if attr in attrs:
+                class_dict[attr] = self.__dict__[attr]
+
+        return class_dict

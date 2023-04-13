82e0d7e8880821beb7808fd0d9de93fcfb2cfb57
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-04-13 00:04:33 +0000

    Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,4 +2,30 @@
 """The module contains a script that reads stdin line by line and computes
 metrics
 """
+import sys
 
+
+count = 0
+while count < 10:
+    total_file_size = 0
+    dictionary = {}
+    for line in sys.stdin:
+
+        splitted_input = line.split()
+
+        if dictionary.get(splitted_input[7]) == None:
+            dictionary[splitted_input[7]] = 1
+        else:
+            dictionary[splitted_input[7]] = (dictionary[splitted_input[7]] + 1)
+
+        count += 1
+        total_file_size += int(splitted_input[8])
+
+        if count == 10:
+            break
+
+    if count == 10:
+        print("File size: {}".format(total_file_size))
+        for key, value in sorted(dictionary.items()):
+            print("{}: {}".format(key, value))
+        count  = 0

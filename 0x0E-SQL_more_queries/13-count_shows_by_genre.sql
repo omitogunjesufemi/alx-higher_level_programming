347270c640a183deb5f8a0c38803541a0e072065
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-05-18 20:51:12 +0000

    A script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,9 @@
 -- A script that lists all genres from hbtn_0d_tvshows and displays the number
 -- of shows linked to each
-SELECT tv_genres.name,
+SELECT tv_genres.name AS 'genre',
        COUNT(tv_show_genres.genre_id)
     AS 'number_of_shows'
   FROM tv_genres, tv_show_genres
  WHERE tv_genres.id = tv_show_genres.genre_id
- GROUP BY tv_genres.name
+ GROUP BY genre
  ORDER BY number_of_shows DESC;

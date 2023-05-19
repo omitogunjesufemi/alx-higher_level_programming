33f37ec81c45c0b50589f6e5e2a5c580e3d5ae5c
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-05-19 12:05:46 +0000

    A script that lists all shows from hbtn_0d_tvshows_rate by their rating.

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- A script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+SELECT title, SUM(rate) AS rating
+  FROM tv_shows, tv_show_ratings
+ WHERE tv_shows.id = tv_show_ratings.show_id
+ GROUP BY title
+ ORDER BY rating DESC;

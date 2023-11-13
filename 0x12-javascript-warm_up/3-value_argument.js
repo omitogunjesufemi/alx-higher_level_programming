130af008a99b95fe428782b724fd866a6d9dbbd5
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-11-14 00:07:15 +0100

    Used process.argv instead

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
 
 let count = 0;
-argv.forEach((val, index) => {
+process.argv.forEach((val, index) => {
   if (index === 2) {
     console.log(val);
   }

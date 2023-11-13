e6fe31d5e9af4b56af76dfca75f7a1d9b9700730
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-11-13 23:54:44 +0100

    Remove import from js file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
 
-if (Object.keys(argv).length === 3) {
+if (process.argv.length === 3) {
   console.log('Argument found');
-} else if (Object.keys(argv).length > 3) {
+} else if (process.argv.length > 3) {
   console.log('Arguments found');
 } else {
   console.log('No argument');

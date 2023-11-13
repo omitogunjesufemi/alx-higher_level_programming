cb1f67846e1548373751b1ac0b87c7e91d296be6
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-11-13 16:39:41 +0100

    Fixed the semistandard for the script

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const {argv} = require('node:process');
+const { argv } = require('node:process');
 
-if (Object.keys(argv).length == 3)
-    console.log('Argument found');
-else if (Object.keys(argv).length > 3)
-    console.log('Arguments found');
-else
-    console.log('No argument');
+if (Object.keys(argv).length === 3) {
+  console.log('Argument found');
+} else if (Object.keys(argv).length > 3) {
+  console.log('Arguments found');
+} else {
+  console.log('No argument');
+}

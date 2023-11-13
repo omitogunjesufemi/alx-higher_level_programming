c8d7aeac42d1bf56bf22024d22e35adf9b3d7c16
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-11-13 23:39:12 +0100

    Script that prints the first arguments passed to it

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,12 +3,12 @@ const { argv } = require('node:process');
 
 let count = 0;
 argv.forEach((val, index) => {
-    if (index === 2) {
-	console.log(val);
-    }
-    count++;
+  if (index === 2) {
+    console.log(val);
+  }
+  count++;
 });
 
 if (count < 3) {
-    console.log("No argument");
+  console.log('No argument');
 }

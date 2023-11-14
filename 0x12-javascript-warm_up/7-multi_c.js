2bf7d1b41a83418f3a5f7d7a330bfe40add424fa
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-11-14 01:00:22 +0100

    A script that prints x times C is fun

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argv = process.argv;
 
-if (argv.length === 3) {
+if (argv.length === 3 && !isNaN(argv[2])) {
   for (let i = 0; i < argv[2]; i++) {
     console.log('C is fun');
   }

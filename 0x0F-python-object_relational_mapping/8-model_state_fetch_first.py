b819629c597727d2e002e515fdf5078520d2c5c9
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-11-18 23:16:16 +0100

    Fixed edge case of when return list is empty

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -20,4 +20,7 @@ if __name__ == "__main__":
     session = Session()
 
     state = session.query(State).first()
-    print("{}: {}".format(state.id, state.name))
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("")

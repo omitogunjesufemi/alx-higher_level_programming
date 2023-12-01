4664b151b479e5b2c2d7dbde7305adc6c895cf51
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-12-01 02:28:49 +0100

    Finds a peak in a list of unsorted integers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -15,7 +15,7 @@ def find_peak(list_of_integers):
     if list_of_integers[0] >= list_of_integers[1]:
         return list_of_integers[0]
 
-    if list_of_integers[-1] >= list_of_integers[-2] :
+    if list_of_integers[-1] >= list_of_integers[-2]:
         return list_of_integers[-1]
 
     low = 1

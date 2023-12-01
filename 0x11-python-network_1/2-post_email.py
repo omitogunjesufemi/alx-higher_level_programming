550445d26fa20cdd2a07b007fb614ffcff7e973c
omitogunjesufemi <omitogunjesufemi@gmail.com>
2023-12-01 17:35:03 +0100

    Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter, and displays the body
+of the response (decoded in utf-8)
+"""
+import sys
+from urllib import request, parse
+
+if __name__ == "__main__":
+    values = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = request.Request(sys.argv[1], data)
+    with urlopen(req) as response:
+        the_page = response.read()
+        print(the_page)

42518353760ddf07a59fde7908023f2a2b5d26ab
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-01-03 19:57:25 +0100

    check if result is empty

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -8,8 +8,9 @@ request(url, (error, response, body) => {
   if (error) console.log(error);
   const allFilms = JSON.parse(body);
 
-  if (allFilms.results.length == 0) return;
+  if (allFilms.results.length === 0) return;
   for (const film of allFilms.results) {
+    if (film.characters.length === 0) return;
     for (const character of film.characters) {
       if (character === 'https://swapi-api.alx-tools.com/api/people/18/') {
         characters.push(character);

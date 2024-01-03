a7bb8089f7670bf81cdb400191fd920d0eace426
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-01-03 14:17:10 +0100

    Print error object if error occured

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,6 +3,6 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 fs.readFile(`./${filePath}`, 'utf-8', (err, data) => {
-  if (err) console.log('Err: ', err);
+  if (err) console.log(err);
   console.log(data);
 });

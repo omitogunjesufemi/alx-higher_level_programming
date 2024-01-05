97a829429f70e34d71426179e6da130df9cfb147
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-01-05 12:47:29 +0100

    return null when an error occurs

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,6 +2,11 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 
+if (!filePath) {
+  console.log('file does not exist');
+  process.exit(1);
+}
+
 fs.readFile(`./${filePath}`, 'utf-8', (err, data) => {
   if (err) {
     console.log(err);

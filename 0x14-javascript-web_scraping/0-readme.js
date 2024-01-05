42170e2e3dac6cc47e986046d836a9ae99a81c4d
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-01-05 12:45:00 +0100

    return null when an error occurs

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,7 +5,7 @@ const filePath = process.argv[2];
 fs.readFile(`./${filePath}`, 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
-    return;
+    process.exit(1);
   }
   console.log(data);
 });
